4eb65675061326434e6804bd0ec9bb823b1e981e
Uthpala Samarakoon <uthpalakasun7@gmail.com>
2024-04-11 00:06:35 +0530

    fixed navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd';
-import FlightPredictionContainer from './Components/FlightPredictionContainer';
-import '../src/SCSS/Index.scss'
-import Home from './Components/Home';
-import Feedback from './Components/Feedback';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
+import FlightPredictionContainer from "./Components/FlightPredictionContainer";
+import "../src/SCSS/Index.scss";
+import Home from "./Components/Home";
+import Feedback from "./Components/Feedback";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/* <Home/> */}
-      <FlightPredictionContainer/>
-      {/* <Feedback/> */}
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="flight-status" element={<FlightPredictionContainer />} />
+        <Route path="feedback" element={<Feedback />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React from "react";
 
 import { Button } from "antd";
 
-import LOGO from '../Assets/nav_logo_1 1.png'
+import LOGO from "../Assets/nav_logo_1 1.png";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-        <nav className='NavBar'>
-            <img src={LOGO} style={{ height: '30px', backgroundSize: 'cover' }}></img>
-            <div className='NavlinkContainer'>
-                <ul className='NavlinkList'>
-                    <li className='Navlink'>Home</li>
-                    <li className='Navlink'>Flight Status</li>
-                    <li className='Navlink'>Feedback</li>
-                </ul>
-                <Button className='LoginBtn' type="primary">LOGIN</Button>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="NavBar">
+      <img
+        alt="logo"
+        src={LOGO}
+        style={{ height: "30px", backgroundSize: "cover" }}
+      ></img>
+      <div className="NavlinkContainer">
+        <ul className="NavlinkList">
+          <li className="Navlink">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="Navlink">
+            <Link to="/flight-status">Flight Status</Link>
+          </li>
+          <li className="Navlink">
+            <Link to="/feedback">Feedback</Link>
+          </li>
+        </ul>
+        <Button className="LoginBtn" type="primary">
+          LOGIN
+        </Button>
+      </div>
+    </nav>
+  );
 }
