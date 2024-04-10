45f3f8c5620cb281b84e4b37c2d9a73203781d5a
Uthpala Samarakoon <uthpalakasun7@gmail.com>
2024-04-11 00:14:13 +0530

    added not found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
 import FlightPredictionContainer from "./Components/FlightPredictionContainer";
 import "../src/SCSS/Index.scss";
 import Home from "./Components/Home";
+import NotFound from "./Components/NotFound";
 import Feedback from "./Components/Feedback";
 import { Route, Routes } from "react-router-dom";
 
@@ -14,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="flight-status" element={<FlightPredictionContainer />} />
         <Route path="feedback" element={<Feedback />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
